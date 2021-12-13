bc531fc922e98d0d3be1b99203809374d2d839dd
19PA1A0515 <60499532+19PA1A0515@users.noreply.github.com>
2021-12-13 10:12:40 +0530

    Create snake.java

---
+++ snake.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Snakes{
+  private int noOfSnakes;
+  private Map<Integer,Integer> headAndTails = new HashMap<Integer,Integer>();
+  public Snakes(int noOfSnakes){
+    this.noOfSnakes=noOfSnakes;
+  }
+  public void addHeadAndTails(int head,int tail){
+    headAndTails.put(head,tail);
+  }
+  public int getNoOfSnakes{
+    return noOfSnakes;
+  }
+  public Map<Integer,Integer> getHeadAndTails{
+    return headAndTails;
+  }
+    
